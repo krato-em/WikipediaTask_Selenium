1beef285354a1bfe94ed30ddb24208c6253c122d
Emilie Kratochvilova <kratochvilova-em@gmail.com>
2024-03-04 00:11:40 +0100

    Add project files.

---
+++ WikipediaTaskOne/Program.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using WikipediaTaskOne;
+using System.Drawing;
+using OpenQA.Selenium;
+
+
+/*********************************************************************************************************************************************
+  
+README:
+
+- This project is build as a console application.
+- Google Chrome is set as a default browser for runnig this project. 
+- If you'd like to use Edge instead, simply uncomment "Edge option" section and comment "Chrome option" section.
+- Run the application and follow the instructions.
+- Feel free to use following URL as an example:
+    https://en.wikipedia.org/wiki/Classical_antiquity
+    https://en.wikipedia.org/wiki/Dolphin
+    https://en.wikipedia.org/wiki/Philosophy
+
+*********************************************************************************************************************************************/
+
+
+Console.WriteLine("Paste any Wikipedia URL below here and I'll tell you how many redirects it takes to get to Philosophy page.");
+Console.Write("Your URL: ");
+string url = Console.ReadLine().Trim();
+
+
+// Chrome option:
+var driver = new ChromeDriver();
+//// Edge option:
+//var driver = new EdgeDriver();
+
+
+WikiPage wiki = new WikiPage(driver);
+driver.Manage().Window.Size = new Size(1920, 1200);
+driver.Navigate().GoToUrl(url);
+
+
+int counter = 0;
+
+while (!wiki.IsThisPhilosophyPage())
+{
+    if (wiki.IsPhilosophyLinkOnThePage())
+    {
+        wiki.ClickPhilosophyLink();
+    }
+    else
+    {
+        wiki.ClickFirstLinkToRedirect();        
+    }
+    counter++;
+}
+driver.Quit();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Number of redirects: {counter}");
+Console.ResetColor();
+
+
---
+++ WikipediaTaskOne/WikiPage.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+
+namespace WikipediaTaskOne
+{
+    internal class WikiPage
+    {
+        protected IWebDriver driver;
+        public WikiPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement ContentPage => driver.FindElement(By.Id("mw-content-text"));
+
+        public bool IsPhilosophyLinkOnThePage()
+        {
+            // Let's get '<a href>' attributes only from paragraphs
+            var paragraphs = ContentPage.FindElements(By.TagName("p")).ToList();
+            bool isPhilosophyLinkHere = false;
+
+            foreach (var paragraph in paragraphs)
+            {
+                // pick only '<a href>' that are not a reference
+                var elements = paragraph.FindElements(By.XPath(".//a[@href and not(ancestor::sup[@class='reference'])]"));
+
+                if (elements.ToList().Count > 0)
+                {
+                    foreach (var attribute in elements)
+                    {
+                        if (attribute.GetAttribute("title") == "Philosophy")
+                        {
+                            isPhilosophyLinkHere = true;
+                            Console.WriteLine("Philosophy found!!!");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (isPhilosophyLinkHere)
+                {
+                    break;
+                }
+            }
+
+            return isPhilosophyLinkHere;
+        }
+
+
+        public void ClickFirstLinkToRedirect()
+        {
+            var paragraphs = ContentPage.FindElements(By.TagName("p")).ToList();
+            foreach (var paragraph in paragraphs)
+            {
+                var firstLink = paragraph.FindElements(By.XPath(".//a[@href and not(ancestor::sup[@class='reference'])]"));
+                if (firstLink.Count != 0)
+                {
+                    firstLink[0].Click();
+                    break;
+                }
+            }
+        }
+
+        public void ClickPhilosophyLink()
+        {
+            var philosophy = driver.FindElements(By.CssSelector("[href*='/wiki/Philosophy']"));
+            philosophy[0].Click();
+            Thread.Sleep(1000);
+        }
+
+        public bool IsThisPhilosophyPage()
+        {
+            var header = driver.FindElement(By.ClassName("mw-page-title-main")).Text;
+            if (header == "Philosophy")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
